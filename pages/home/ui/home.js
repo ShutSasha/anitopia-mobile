cf865f1a8663084d3b28b88e409dd9699a69a80d
Sasha Shut <sanyashut159@gmail.com>
2024-05-26 17:30:34 +0300

    added user img

--- pages/home/ui/home.js
+++ pages/home/ui/home.js
@@ -1,18 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { FlatList } from 'react-native'
 import { Header } from '../../../widgets/header'
 import { UpdatedAnime } from '../../../widgets/updated-anime'
 import { ReleasedAnime } from '../../../widgets/released-anime'
 import { AnimeSeason } from '../../../widgets/anime-season'
-import { useStore } from '../../../hooks/useStore'
-import { toJS } from 'mobx'
 
 export const Home = () => {
-   const { store } = useStore()
-   useEffect(() => {
-      console.log(toJS(store.user))
-   }, [store.user])
-
    return (
       <>
          <Header />
--- widgets/header/ui/Header.js
+++ widgets/header/ui/Header.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView, Image, Text, View, TouchableOpacity } from 'react-native'
 import { styles } from './styles'
 import NotificationIcon from '../../../assets/notification-icon.svg'
+import { useStore } from '../../../hooks/useStore'
+import { observer } from 'mobx-react-lite'
 
-export const Header = () => {
+export const Header = observer(() => {
+   const { store } = useStore()
+
    return (
       <SafeAreaView style={styles.container}>
          <View style={styles.blockWithElements}>
@@ -12,9 +16,12 @@ export const Header = () => {
             </TouchableOpacity>
             <Text style={styles.name}>Anitopia</Text>
             <TouchableOpacity onPress={() => console.log('Профіль')}>
-               <Image style={styles.avatar} source={require('../../../assets/avatar.jpg')} />
+               <Image
+                  style={styles.avatar}
+                  source={store.user ? { uri: store.user.avatarLink } : require('../../../assets/avatar.jpg')}
+               />
             </TouchableOpacity>
          </View>
       </SafeAreaView>
    )
-}
+})
